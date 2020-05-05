fd58de77e9a60b9666607900b71deb436edc189b
Dipsy974 <mathieujulie974@hotmail.com>
2020-05-05 09:55:52 +0400

    Page transitions added

--- apps/app.js
+++ apps/app.js
@@ -1,6 +1,13 @@
+//for animateSlide
 let scrollController;
 let slideScene;
 let pageScene;
+//for cursor
+const mouse = document.querySelector(".cursor");
+const mouseTxt = mouse.querySelector("span");
+const burger = document.querySelector(".burger");
+//for logo in page transitions
+const logo = document.querySelector("#logo");
 
 function animateSlides() {
   scrollController = new ScrollMagic.Controller();
@@ -17,7 +24,6 @@ function animateSlides() {
     slideTl.fromTo(revealImg, { x: "0%" }, { x: "100%" });
     slideTl.fromTo(img, { scale: 2 }, { scale: 1 }, "-=1");
     slideTl.fromTo(revealTxt, { x: "0%" }, { x: "100%" }, "-=0.65");
-    slideTl.fromTo(nav, { y: "-100%" }, { y: "0%" }, "-=0.60");
 
     //Create scene
     slideScene = new ScrollMagic.Scene({
@@ -56,8 +62,35 @@ function animateSlides() {
   });
 }
 
-let mouse = document.querySelector(".cursor");
-let mouseTxt = mouse.querySelector("span");
+function animateFashion() {
+  scrollController = new ScrollMagic.Controller();
+  const slides = document.querySelectorAll(".detail-slide");
+  slides.forEach((slide, index, slides) => {
+    const slideTl = gsap.timeline({
+      defaults: { duration: 1, ease: "power2.inOut" },
+    });
+    let nextSlide = slides.length - 1 === index ? "end" : slides[index + 1];
+    const nextImg = nextSlide.querySelector("img");
+    slideTl.fromTo(slide, { opacity: 1 }, { opacity: 0 });
+    slideTl.fromTo(nextSlide, { opacity: 0 }, { opacity: 1 }, "-=1");
+    slideTl.fromTo(nextImg, { x: "50%" }, { x: "0%" }, "-=1");
+
+    slideScene = new ScrollMagic.Scene({
+      triggerElement: slide,
+      duration: "100%",
+      triggerHook: 0,
+    })
+      .setPin(slide, { pushFollowers: false })
+      .setTween(slideTl)
+      .addIndicators({
+        colorStart: "white",
+        colorTrigger: "white",
+        name: "slide",
+      })
+      .addTo(scrollController);
+  });
+}
+
 function cursor(e) {
   mouse.style.top = e.pageY + "px";
   mouse.style.left = e.pageX + "px";
@@ -81,6 +114,81 @@ function activeCursor(e) {
   }
 }
 
+function navToggle(event) {
+  if (!event.target.classList.contains("active")) {
+    event.target.classList.add("active");
+    gsap.to(".line1", 0.5, { rotate: "45", y: "5", background: "black" });
+    gsap.to(".line2", 0.5, { rotate: "-45", y: "-5", background: "black" });
+    gsap.to("#logo", 0.5, { color: "black" });
+    gsap.to(".nav-bar", 1, { clipPath: "circle(2300px at 100% -10%)" });
+    document.body.classList.add("hide");
+  } else {
+    event.target.classList.remove("active");
+    gsap.to(".line1", 0.5, { rotate: "0", y: "0", background: "white" });
+    gsap.to(".line2", 0.5, { rotate: "0", y: "0", background: "white" });
+    gsap.to("#logo", 0.5, { color: "white" });
+    gsap.to(".nav-bar", 1, { clipPath: "circle(50px at 100% -10%)" });
+    document.body.classList.remove("hide");
+  }
+}
+
+//EVENT LISTENERS
 window.addEventListener("mousemove", cursor);
 window.addEventListener("mouseover", activeCursor);
-animateSlides();
+burger.addEventListener("click", navToggle);
+
+barba.init({
+  views: [
+    {
+      namespace: "home",
+      beforeEnter() {
+        animateSlides();
+        logo.href = "./index.html";
+      },
+      beforeLeave() {
+        slideScene.destroy();
+        pageScene.destroy();
+        scrollController.destroy();
+      },
+    },
+    {
+      namespace: "fashion",
+      beforeEnter() {
+        animateFashion();
+        logo.href = "../index.html";
+      },
+      beforeLeave() {
+        slideScene.destroy();
+        scrollController.destroy();
+      },
+    },
+  ],
+  transitions: [
+    {
+      leave({ current, next }) {
+        let done = this.async();
+        const tl = gsap.timeline({ defaults: { ease: "power2.inOut" } });
+        tl.fromTo(current.container, 1, { opacity: 1 }, { opacity: 0 });
+        tl.fromTo(
+          ".swipe",
+          0.75,
+          { x: "-100%" },
+          { x: "0%", onComplete: done },
+          "-=0.5"
+        );
+      },
+      enter({ current, next }) {
+        let done = this.async();
+        window.scrollTo(0, 0);
+        const tl = gsap.timeline({ defaults: { ease: "power2.inOut" } });
+        tl.fromTo(
+          ".swipe",
+          0.75,
+          { x: "0%" },
+          { x: "100%", stagger: 0.25, onComplete: done }
+        );
+        tl.fromTo(next.container, 1, { opacity: 0 }, { opacity: 1 });
+      },
+    },
+  ],
+});
